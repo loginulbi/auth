e3dde940336741dc0434ca2883d798719273181c
ayalarifki <ayalarifki@gmail.com>
2024-09-04 13:52:14 +0700

    update redirect

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,5 @@ import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 let token = getCookie("login");
 if (token === "") {
-  window.location.replace("/");
+  window.location.replace("https://login.ulbi.ac.id/");
 }
