3a069b5cda3b48f0d9c896c1ee915b9db09c88ca
GilangAndhika <gryzlegrizz@gmail.com>
2024-09-05 08:35:37 +0700

    Preloader function

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
+// Preloader
+$(window).on("load", function (event) {
+  $(".preloader").delay(500).fadeOut("slow");
+});
+
 let token = getCookie("login");
 if (token === "") {
   window.location.replace("https://login.ulbi.ac.id/");
