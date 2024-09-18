a94f616b4e1fdf59c696bfe333c2702c3e166b1d
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-18 15:03:35 +0700

    refactor: Remove unused URL JavaScript import and update cookie functionality for simpelbi

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { onClick } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/element.js';
 import { redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
 
@@ -32,26 +31,4 @@ export function setCookieWithExpireHour(cname, cvalue, exhour) {
 
   document.cookie = cname + "=" + cvalue + ";" + expires + ";" + domain + ";path=/";
   console.log(`Cookie set: ${cname}=${cvalue}; ${expires}; ${domain}; path=/`);
-=======
-import { setCookieWithExpireHour } from 'https://jscroot.github.io/cookie/croot.js';
-import { redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
-
-// Fungsi untuk menyimpan URL dan redirect ke halaman login
-document.getElementById("simpelbiCard").addEventListener("click", function(event) {
-  event.preventDefault();  // Mencegah default action dari link
-  
-  const targetUrl = this.href;  // URL dari link yang diklik
-  setCookieWithExpireHour('redirect', targetUrl, 1);  // Menyimpan URL ke cookie "redirect" dengan durasi 1 jam
-  
-  redirect('https://login.ulbi.ac.id');  // Redirect ke halaman login setelah menyimpan URL
-});
-
-// Fungsi untuk menyimpan cookie dengan batas waktu (dalam jam)
-function setCookieWithExpireHour(cname, cvalue, exhour) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
-  const expires = "expires=" + d.toUTCString();
-  document.cookie = `${cname}=${cvalue};${expires};domain=.ulbi.ac.id;path=/`;
-  console.log(`Cookie disetel: ${cname}=${cvalue}`);
->>>>>>> f41aae3eefd2019771793b691e3f522388d4130a
 }
