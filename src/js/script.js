bcef166475dd316dc5d39becd1bc7e02aaea0ee6
GilangAndhika <gryzlegrizz@gmail.com>
2024-09-18 14:31:08 +0700

    Add cookie redirect

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
 
 let token = getCookie("login");
 if (token === "") {
   window.location.replace("https://login.ulbi.ac.id/");
+  redirect("https://login.ulbi.ac.id/");
 }
