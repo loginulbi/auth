3c6e797189374b75152a856c1feeab5ded99cd53
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-18 15:02:01 +0700

    refactor: Update HRIS link and handle redirect with cookie

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,34 @@
-// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-// import { redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
-
-// let token = getCookie("login");
-// if (token === "") {
-//   window.location.replace("https://login.ulbi.ac.id/");
-//   redirect("https://login.ulbi.ac.id/");
-// }
-
-// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-// import { onClick } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/element.js';
-// import { redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
-
-// let token = getCookie("login");
-// if (token === "") {
-//   window.location.replace("https://login.ulbi.ac.id/");
-// }
-
-// onClick('HRIS', LoginHris);
-// function LoginHris() {
-//   setCookieWithExpireHour('redirect', window.location.href, 1);
-//   redirect('https://login.ulbi.ac.id/');
-// }
-
-// export function setCookieWithExpireHour(cname, cvalue, exhour) {
-//   const d = new Date();
-//   d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
-//   let expires = "expires=" + d.toUTCString();
-  
-//   // Set domain to ulbi.ac.id to allow subdomains access
-//   let domain = "domain=.ulbi.ac.id"; 
-
-//   document.cookie = cname + "=" + cvalue + ";" + expires + ";" + domain + ";path=/";
-// }
-
-import { setCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { onClick } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/element.js';
 import { redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
 
-document.getElementById("simpelbiCard").addEventListener("click", function(event) {
-  event.preventDefault();  // Mencegah redirect default
+onClick('HRIS', () => handleRedirect('HRIS'));
 
-  var redirectUrl = this.href;
-  setCookie("redirect", redirectUrl, 7);  // Menyimpan URL ke dalam cookie selama 7 hari
+function handleRedirect(id) {
+  let redirectUrl = '';
 
-  // Gunakan fungsi redirect dari library eksternal
-  redirect(redirectUrl);
-});
+  // Buat kondisi untuk menetapkan cookie redirect sesuai dengan ID
+  if (id === 'HRIS') {
+    redirectUrl = 'https://euis.ulbi.ac.id/hris-dev/app/validasi-data.html';
+  } else {
+    // Anda bisa menambahkan kondisi lain jika ada ID tambahan
+    redirectUrl = window.location.href;  // Default ke halaman saat ini
+  }
+
+  // Set cookie redirect berdasarkan ID yang dipilih
+  setCookieWithExpireHour('redirect', redirectUrl, 1);
+  
+  // Redirect ke halaman login
+  redirect('https://login.ulbi.ac.id/');
+}
+
+export function setCookieWithExpireHour(cname, cvalue, exhour) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  
+  // Set domain to ulbi.ac.id to allow subdomains access
+  let domain = "domain=.ulbi.ac.id"; 
+
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";" + domain + ";path=/";
+  console.log(`Cookie set: ${cname}=${cvalue}; ${expires}; ${domain}; path=/`);
+}
